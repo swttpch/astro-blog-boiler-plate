60bb6b64bdaa55636b4ff616a861c3ae9457b9a4
swttpch <igor.souza@ecto.digital>
2024-05-26 14:55:02 -0300

    chore: adding primary colors

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#EEF0FF',
+          100: '#E0E4FF',
+          200: '#C7CCFE',
+          300: '#A6ACFB',
+          400: '#8482F7',
+          500: '#7367F0',
+          600: '#6147E4',
+          700: '#5439C9',
+          800: '#4431A2',
+          900: '#3B2F80',
+          950: '#231B4B',
+        },
+      },
+    },
   },
   plugins: [],
 };
