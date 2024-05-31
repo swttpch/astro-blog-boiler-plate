82aa8ba9bff7aad5b4439c4cee8518aca187e074
swttpch <igor.souza@ecto.digital>
2024-05-31 19:29:57 -0300

    test

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,37 +1,12 @@
 module.exports = [
-  "strapi::logger",
-  "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "https:",
-            "ecto-hub.s3.us-east-1.amazonaws.com",
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "https:",
-            "ecto-hub.s3.us-east-1.amazonaws.com",
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
