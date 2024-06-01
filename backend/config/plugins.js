61872dcda7af78ebe20c7c68d5a2e1d27dce8339
swttpch <igor.souza@ecto.digital>
2024-05-31 21:36:29 -0300

    chore: adding aws config of production

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,4 +1,4 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
   graphql: {
     config: {
       endpoint: "/graphql",
@@ -8,6 +8,28 @@ module.exports = () => ({
       amountLimit: 100,
       apolloServer: {
         tracing: false,
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: env("NODE_ENV") === "production" ? "aws-s3" : "local",
+      providerOptions: {
+        s3Options: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: env("AWS_REGION"),
+          params: {
+            ACL: env("AWS_ACL", "public-read"),
+            Bucket: env("AWS_BUCKET_NAME"),
+          },
+        },
+        localServer: {maxage: 300000}
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
